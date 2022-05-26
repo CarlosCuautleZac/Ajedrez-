e71b6191136399a6581e77fd76bded8deaedc226
191G0239@rcarbonifera.tecnm.mx <191G0239@rcarbonifera.tecnm.mx>
2022-05-26 04:11:43 -0500

    cambios

---
+++ Helpers/PositionLeftConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Ajedrez.Views;
+
+namespace Ajedrez.Helpers
+{
+    internal class PositionLeftConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
